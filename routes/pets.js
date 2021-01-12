e149db96f65834cde19861b3c7104dbdced37792
CHO-cmd <wjdqls9362@gmail.com>
2021-01-12 15:47:39 +0900

    completed feature/#19/register and add middlesware multer.js

--- routes/pets.js
+++ routes/pets.js
@@ -2,11 +2,19 @@
 
 const express = require("express");
 const petsController = require("../controller");
+const { upload } = require("./multer");
+const passport = require("passport");
+
 const router = express.Router();
 
 router.get("/petslist", petsController.pets.petslist);
 router.get("/detail/:id", petsController.pets.detail);
-router.post("/register", petsController.pets.register);
+router.post(
+  "/register",
+  passport.authenticate("jwt", { session: false }),
+  upload.array("img"),
+  petsController.pets.register
+);
 router.put("/edit", petsController.pets.edit);
 router.delete("/remove/:id", petsController.pets.remove);
 router.get("/search", petsController.pets.search);
