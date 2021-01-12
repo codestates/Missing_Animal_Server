cd9e84dff93f79cdb256e17b3b3dd95b61cd8630
CHO-cmd <wjdqls9362@gmail.com>
2021-01-12 13:23:59 +0900

    completed pets/petslist

--- controller/pets/petslist.js
+++ controller/pets/petslist.js
@@ -1,22 +1,23 @@
-const axios = require("axios");
+const { Pets } = require("../../models");
+const pets = require("../../models/pets");
 
-module.exports = (req, res) => {
-  res.status(200).json({
-    petslist: [
-      {
-        petsId: "petsId",
-        thumbnail: "thumbnail",
-        title: "title",
-        petname: "petname",
-        description: "description",
-      },
-      {
-        petsId: "petsId",
-        thumbnail: "thumbnail",
-        title: "title",
-        petname: "petname",
-        description: "description",
-      },
-    ],
+module.exports = async (req, res) => {
+  const petsData = await Pets.findAll({
+    attributes: ["id", "thumbnail", "title", "petname", "description"],
+    order: [["createdAt", "DESC"]],
   });
+
+  const petslist = [];
+  for (let i = 0; i < petsData.length; i += 1) {
+    const onePet = {
+      petsId: petsData[i].id,
+      thumbnail: petsData[i].thumbnail,
+      title: petsData[i].title,
+      petname: petsData[i].petname,
+      description: petsData[i].description,
+    };
+    petslist.push(onePet);
+  }
+
+  res.status(200).json({ petslist });
 };
