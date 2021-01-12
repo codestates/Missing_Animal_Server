686325092fe5037f4a78222be5f33ba614a5f006
CHO-cmd <wjdqls9362@gmail.com>
2021-01-12 12:35:43 +0900

    completed users/myinfo

--- controller/users/myinfo.js
+++ controller/users/myinfo.js
@@ -1,13 +1,34 @@
 const axios = require("axios");
+const { Users, Pets } = require("../../models");
+const pets = require("../../models/pets");
 
-module.exports = (req, res) => {
-  console.log("users:", req.user);
-  res.status(201).json({
-    userId: "userId",
-    username: "username",
-    email: "email",
-    mobile: "mobile",
-    petsId: ["petsId", "petsId"],
-    createdAt: "createdAt",
+module.exports = async (req, res) => {
+  const { id } = req.user;
+
+  const userinfo = await Users.findAll({
+    where: { id },
+    attributes: ["id", "username", "email", "mobile", "createdAt"],
+    include: [
+      {
+        model: Pets,
+        attributes: ["id"], // 추후에 추가
+      },
+    ],
   });
+
+  const petsId = [];
+  for (let i = 0; i < userinfo[0].pets.length; i += 1) {
+    petsId.push(userinfo[0].pets[i].id);
+  }
+
+  const data = {
+    userId: userinfo[0].id,
+    username: userinfo[0].username,
+    email: userinfo[0].email,
+    mobile: userinfo[0].mobile,
+    petsId,
+    createdAt: userinfo[0].createdAt,
+  };
+
+  res.status(200).json(data);
 };
