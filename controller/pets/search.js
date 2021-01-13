498a8a6867913abee29869a5bfbe27d4eda2cef1
psy-g <psykyg@gmail.com>
2021-01-13 16:50:41 +0900

    completed feature/#22/search

--- controller/pets/search.js
+++ controller/pets/search.js
@@ -1,22 +1,49 @@
-const axios = require("axios");
+const { Pets } = require("../../models");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
 
-module.exports = (req, res) => {
-  res.status(200).json({
-    filteredList: [
-      {
-        petsId: "petsId",
-        thumbnail: "thumbnail",
-        title: "title",
-        petname: "petname",
-        description: "description",
+module.exports = async (req, res) => {
+  const { search } = req.body;
+
+  const filterPets = await Pets.findAll({
+    where: {
+      [Op.or]: {
+        title: {
+          [Op.like]: "%" + search + "%",
+        },
+        petname: {
+          [Op.like]: "%" + search + "%",
+        },
+        area: {
+          [Op.like]: "%" + search + "%",
+        },
+        sex: {
+          [Op.like]: "%" + search + "%",
+        },
+        species: {
+          [Op.like]: "%" + search + "%",
+        },
+        reward: {
+          [Op.like]: "%" + search + "%",
+        },
+        description: {
+          [Op.like]: "%" + search + "%",
+        },
       },
-      {
-        petsId: "petsId",
-        thumbnail: "thumbnail",
-        title: "title",
-        petname: "petname",
-        description: "description",
-      },
-    ],
+    },
   });
+
+  const filterList = [];
+  for (let i = 0; i < filterPets.length; i += 1) {
+    const filter = {
+      petsId: filterPets[i].id,
+      thumbnail: filterPets[i].thumbnail,
+      title: filterPets[i].title,
+      petname: filterPets[i].petname,
+      description: filterPets[i].description,
+    };
+    filterList.push(filter);
+  }
+
+  res.status(200).json({ filteredList: filterList });
 };
