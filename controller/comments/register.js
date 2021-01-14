16c5690456171fd152b04afa189baa827f5c5863
psy-g <psykyg@gmail.com>
2021-01-14 20:26:02 +0900

    completed controller/comments/register.js

--- controller/comments/register.js
+++ controller/comments/register.js
@@ -1,13 +1,19 @@
-const { Comments, Users } = require("../../models");
+const { Comments } = require("../../models");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
-  const { text, nick, password, petId, userId } = req.body;
+  const { text, nick, password, petId } = req.body;
+
+  const token = req.headers.authorization.replace("Bearer ", "");
+  const tokenDecoded = jwt.decode(token, process.env.JWT_SECRET);
+
+  console.log("tokenDecoded:", tokenDecoded);
 
   // 넘어오는 userId가 user 테이블에 있는지 확인
-  const users = await Users.findAll();
-  const userIdList = users.map((obj) => obj.dataValues.id);
+  // const users = await Users.findAll();
+  // const userIdList = users.map((obj) => obj.dataValues.id);
 
-  console.log("userIdList:", userIdList);
+  // console.log("userIdList:", userIdList);
 
   // 비회원 댓글
   // petId가 안넘어오면 failed
@@ -31,15 +37,15 @@ module.exports = async (req, res) => {
 
     // 회원 댓글
     // userId가 안넘어오면 failed
-    else if (!userId) {
+    else if (!tokenDecoded.id) {
       return res.status(400).json({ message: "required userId" });
     } else {
       await Comments.create({
         image: req.files[0].path,
         text,
-        nick: null,
+        nick: tokenDecoded.username,
         password: null,
-        userId: userId,
+        userId: tokenDecoded.id,
         petId,
       });
       return res.status(201).json({
@@ -48,19 +54,3 @@ module.exports = async (req, res) => {
     }
   }
 };
-
-// for (let i = 0; i < userIdList.length; i++) {
-//   if (userId === userIdList[i]) {
-//     await Comments.create({
-//       image: req.files[0].path,
-//       text,
-//       nick: null,
-//       password: null,
-//       userId: userId,
-//       petId,
-//     });
-//     return res.status(201).json({
-//       message: "register Ok",
-//     });
-//   }
-// }
