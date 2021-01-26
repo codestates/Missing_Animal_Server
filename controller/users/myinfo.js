f9018a2c1843e959348b221b7853bf37c7ab269d
CHO-cmd <wjdqls9362@gmail.com>
2021-01-26 11:30:37 +0900

    modified users/myinfo.js

--- controller/users/myinfo.js
+++ controller/users/myinfo.js
@@ -3,7 +3,8 @@ const { Users, Pets, PetsImages } = require("../../models");
 module.exports = async (req, res) => {
   const { id } = req.user;
 
-  const userinfo = await Pets.findAll({
+  const myinfo = await Users.findOne({ where: { id } });
+  const myPetInfo = await Pets.findAll({
     where: { userId: id },
     attributes: [
       "id",
@@ -25,49 +26,47 @@ module.exports = async (req, res) => {
         model: PetsImages,
         attributes: ["imagePath"],
       },
-      {
-        model: Users,
-        attributes: ["id", "username", "email", "mobile", "createdAt"],
-      },
     ],
   });
 
-  // console.log(userinfo[0].petsImages);
+  console.log(myPetInfo);
 
-  const petslist = [];
-  for (let i = 0; i < userinfo.length; i += 1) {
-    const imageArr = [];
-    for (let j = 0; j < userinfo[i].petsImages.length; j += 1) {
-      const imagePath = {
-        imagePath: userinfo[i].petsImages[j].imagePath,
+  let petslist = [];
+  if (myPetInfo.length) {
+    for (let i = 0; i < myPetInfo.length; i += 1) {
+      const imageArr = [];
+      for (let j = 0; j < myPetInfo[i].petsImages.length; j += 1) {
+        const imagePath = {
+          imagePath: myPetInfo[i].petsImages[j].imagePath,
+        };
+        imageArr.push(imagePath);
+      }
+
+      const oneObj = {
+        id: myPetInfo[i].id,
+        title: myPetInfo[i].title,
+        petname: myPetInfo[i].petname,
+        area: myPetInfo[i].area,
+        sex: myPetInfo[i].sex,
+        missingDate: myPetInfo[i].missingDate,
+        description: myPetInfo[i].description,
+        species: myPetInfo[i].species,
+        reward: myPetInfo[i].reward,
+        latitude: myPetInfo[i].latitude,
+        longitude: myPetInfo[i].longitude,
+        createdAt: myPetInfo[i].createdAt,
+        petsImages: imageArr,
       };
-      imageArr.push(imagePath);
+      petslist.push(oneObj);
     }
-
-    const oneObj = {
-      id: userinfo[i].id,
-      title: userinfo[i].title,
-      petname: userinfo[i].petname,
-      area: userinfo[i].area,
-      sex: userinfo[i].sex,
-      missingDate: userinfo[i].missingDate,
-      description: userinfo[i].description,
-      species: userinfo[i].species,
-      reward: userinfo[i].reward,
-      latitude: userinfo[i].latitude,
-      longitude: userinfo[i].longitude,
-      createdAt: userinfo[i].createdAt,
-      petsImages: imageArr,
-    };
-    petslist.push(oneObj);
   }
 
   const data = {
-    userId: userinfo[0].user.id,
-    username: userinfo[0].user.username,
-    email: userinfo[0].user.email,
-    mobile: userinfo[0].user.mobile,
-    createdAt: userinfo[0].user.createdAt,
+    userId: myinfo.id,
+    username: myinfo.username,
+    email: myinfo.email,
+    mobile: myinfo.mobile,
+    createdAt: myinfo.createdAt,
     petslist,
   };
 
